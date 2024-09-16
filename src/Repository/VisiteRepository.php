94ad463f0e4cfddc6b6678108e9ea37db5de2cd1
john doe <devblocks42@keemail.me>
2024-09-16 19:13:45 +0200

    ORM (affichage des informations provenant de la BDD).

---
+++ src/Repository/VisiteRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Visite;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Visite>
+ */
+
+/**
+ * @method Visite|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Visite|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Visite[]    findAll()
+ * @method Visite[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class VisiteRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Visite::class);
+    }
+}
