873b4d40f80784097b7a75406d27af7bd56a1ebf
john doe <devblocks42@keemail.me>
2024-09-18 19:21:44 +0200

    ORM (gestion du lien entre les tables, partie admin).

--- src/Form/VisiteType.php
+++ src/Form/VisiteType.php
@@ -3,10 +3,12 @@
 namespace App\Form;
 
 use App\Entity\Visite;
+use App\Entity\Environnement;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class VisiteType extends AbstractType
 {
@@ -25,6 +27,12 @@ class VisiteType extends AbstractType
             ])
             ->add('tempmax', null, [
                 'label'=> 't° max'
+            ])
+            ->add('environnements', EntityType::class, [
+                'class'=> Environnement::class,
+                'choice_label' => "nom",
+                'multiple' => true,
+                'required' => false
             ])
             ->add('pays')
             ->add('submit', SubmitType::class, [
